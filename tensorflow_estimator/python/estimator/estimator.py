e3eb2e591745cd2c086e4583d8337f35f49eda42
styagi <styagi@iu.edu>
2020-02-11 23:31:08 -0500

    trying to fix tensor doesn't exist issue

--- tensorflow_estimator/python/estimator/estimator.py
+++ tensorflow_estimator/python/estimator/estimator.py
@@ -1501,9 +1501,11 @@ class Estimator(object):
         #    e1 = time.time()
         #    logging.info('@sahiltyagi COMPUTE GRAD ITERTIME ON WORKER IS ' + str(e1 - s1) + ' with starttime ' + str(s1) + ' and endtime ' + str(e1) + ' and global step ' + str(glob_step))
         starttime = time.time()
-        _, loss, curr_step = mon_sess.run([estimator_spec.train_op, estimator_spec.loss, tf.train.get_or_create_global_step()])
+        _, loss, curr_step, grad_starttime, grad_endtime = mon_sess.run([estimator_spec.train_op, estimator_spec.loss, tf.train.get_or_create_global_step(),
+                                                                         tf.get_default_graph().get_tensor_by_name("resnet/tower_0/grad_starttime:0"),
+                                                                         tf.get_default_graph().get_tensor_by_name("resnet/tower_0/grad_endtime:0")])
         endtime = time.time()
-        grad_starttime, grad_endtime = mon_sess.run([tf.get_default_graph().get_tensor_by_name("grad_starttime:0"), tf.get_default_graph().get_tensor_by_name("grad_endtime:0")])
+        #grad_starttime, grad_endtime = mon_sess.run([tf.get_default_graph().get_tensor_by_name("resnet/tower_0/grad_starttime:0"), tf.get_default_graph().get_tensor_by_name("resnet/tower_0/grad_endtime:0")])
         logging.info('@sahiltyagi iteration time on given worker is ' + str(endtime - starttime) + ' with starttime ' + str(starttime) + ' and endtime ' + str(endtime) + ' and global step ' + str(curr_step))
         logging.info('@sahiltyagi COMPUTEGRAD ITERTIME WITH START END TENSOR ' + str(grad_endtime[0] - grad_starttime[0]) + ' with starttime ' + str(grad_starttime[0]) + ' and endtime ' + str(grad_endtime[0]) + ' and global step ' + str(curr_step))
         any_step_done = True
