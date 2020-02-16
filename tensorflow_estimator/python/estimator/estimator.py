00d6cbbe2346ee957669be476af59d1c04d34463
styagi <styagi@iu.edu>
2020-02-16 12:44:55 -0500

    adding compute_gradients as a separate op and using that output to feed data to apply_gradients call

--- tensorflow_estimator/python/estimator/estimator.py
+++ tensorflow_estimator/python/estimator/estimator.py
@@ -1547,6 +1547,7 @@ class Estimator(object):
         logging.info('......................just finished running compute gradients op.......')
         logging.info(type(gradvar_values))
         logging.info(len(gradvar_values))
+        logging.info(gradvar_values)
 
         # grad_vals = mon_sess.run([grad for grad,_ in gradvar_values])
         grad_compute_endtime = time.time()
