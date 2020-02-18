7d59d9376407c2a55fe47bbb29cf48dbc52cee98
styagi <styagi@iu.edu>
2020-02-17 19:39:11 -0500

    using py_func to get gradient start time only

--- tensorflow_estimator/python/estimator/estimator.py
+++ tensorflow_estimator/python/estimator/estimator.py
@@ -1497,13 +1497,13 @@ class Estimator(object):
           #                                                      tf.get_default_graph().get_tensor_by_name('resnet/tower_0/START_SAHIL_TIME_GRADIENT:0').eval(),
           #                                                      tf.get_default_graph().get_tensor_by_name('resnet/tower_0/END_SAHIL_TIME_GRADIENT:0').eval()])
 
-          _, loss, curr_step, cg_start = mon_sess.run([estimator_spec.train_op, estimator_spec.loss, tf.train.get_or_create_global_step(),
-                                                        tf.get_default_graph().get_tensor_by_name('resnet/tower_0/START_SAHIL_TIME_GRADIENT:0')])
+          _, loss, curr_step  = mon_sess.run([estimator_spec.train_op, estimator_spec.loss, tf.train.get_or_create_global_step()])
           step_end = time.time()
           any_step_done = True
           logging.info('@sahiltyagi train_op iteration time given worker is ' + str(step_end - step_start) + ' with starttime ' + str(step_start)
                        + ' and endtime ' + str(step_end) + ' and global step ' + str(curr_step))
-          logging.info('@sahiltyagi COMPUTE GRAD time on worker is ' + str(cg_start) + ' and current step is ' + str(curr_step))
+          logging.info('@sahiltyagi COMPUTE GRAD time on worker is ' + str(tf.get_default_graph().get_tensor_by_name('resnet/tower_0/START_SAHIL_TIME_GRADIENT:0'))
+                       + ' and current step is ' + str(curr_step))
 
           # logging.info('@sahiltyagi COMPUTE GRAD time on worker is ' + str(float(cg_end) - float(cg_start)) + ' with starttime ' + str(cg_start)
           #              + ' and endtime ' + str(cg_end) + ' and global step ' + str(curr_step))
