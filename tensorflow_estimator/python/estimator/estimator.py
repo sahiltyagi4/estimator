dacda6204037afc60ed6802db3fff52053cb8721
sahiltyagi4 <sahilt.tyagi@gmail.com>
2020-07-13 11:27:11 -0400

    returning gradvars from apply_grad call

--- tensorflow_estimator/python/estimator/estimator.py
+++ tensorflow_estimator/python/estimator/estimator.py
@@ -1540,9 +1540,7 @@ class Estimator(object):
           # grad_var3 = mon_sess.run([tf.get_variable('agg_grads_variance0')])
           # logging.info('@sahiltyagi4 aggregated gradient variance1 is ' + str(grad_var3) + ' for global step ' + str(curr_step))
 
-          mon_sess.run([tf.get_default_graph().get_operation_by_name('sync_replicas/xyz_test_assignment')])
-          logging.info('@sahiltyagi4 done running the op for assignment!!!!')
-          grad_var4 = mon_sess.run([tf.get_default_graph().get_tensor_by_name("agg_grads_variance0:0")])
+          grad_var4 = mon_sess.run([tf.get_default_graph().get_tensor_by_name('sync_replicas/xyz_test_assignment:0')])
           logging.info('@sahiltyagi4 aggregated gradient variance1 is ' + str(grad_var4) + ' for global step ' + str(curr_step))
 
           #mon_sess.run(tf.get_default_graph().get_tensor_by_name("agg_grads_variance0:0"))
