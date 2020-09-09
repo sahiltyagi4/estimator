81e9b41ede5fd5788bd7d1085484fe75bdc06123
sahiltyagi4 <sahilt.tyagi@gmail.com>
2020-09-09 17:01:24 -0400

    added transformers input fn

--- tensorflow_estimator/python/estimator/training.py
+++ tensorflow_estimator/python/estimator/training.py
@@ -863,16 +863,26 @@ class _TrainingExecutor(object):
       elif 'regression' in workload:
         logging.info('@sahiltyagi4 going to use workload ' + workload)
         new_input_fn = functools.partial(switched_input_fn, batchsize=new_batch_size)
+        logging.info('@sahiltyagi4 value set for new batch-size on switched input fn is {}'.format(new_batch_size))
+        loss, should_switch_input_fn = self._estimator.train(
+            input_fn=new_input_fn,
+            max_steps=self._train_spec.max_steps,
+            hooks=list(self._train_spec.hooks) + list(self._train_hooks),
+            saving_listeners=saving_listeners)
+
       elif 'mnist_cnn' in workload:
         logging.info('@sahiltyagi4 going to use workload ' + workload)
         new_input_fn = functools.partial(switched_input_fn)
-
-      logging.info('@sahiltyagi4 value set for new batch-size on switched input fn is {}'.format(new_batch_size))
-      loss, should_switch_input_fn = self._estimator.train(
-          input_fn=new_input_fn,
-          max_steps=self._train_spec.max_steps,
-          hooks=list(self._train_spec.hooks) + list(self._train_hooks),
-          saving_listeners=saving_listeners)
+        logging.info('@sahiltyagi4 value set for new batch-size on switched input fn is {}'.format(new_batch_size))
+        loss, should_switch_input_fn = self._estimator.train(input_fn=new_input_fn,
+                                                             max_steps=self._train_spec.max_steps,
+                                                             hooks=list(self._train_spec.hooks) + list(self._train_hooks),
+                                                             saving_listeners=saving_listeners)
+      elif 'transformers' in workload:
+        loss, should_switch_input_fn = self._estimator.train(input_fn=self._train_spec.input_fn,
+                                                             max_steps=self._train_spec.max_steps,
+                                                             hooks=list(self._train_spec.hooks) + list(self._train_hooks),
+                                                             saving_listeners=saving_listeners)
 
       # loss = self._estimator.train(
       #     input_fn=self._train_spec.input_fn,
