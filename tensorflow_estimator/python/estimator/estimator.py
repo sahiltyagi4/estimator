1af54c7d6f179288a935a742ed1af4a758637e5a
sahiltyagi4 <sahilt.tyagi@gmail.com>
2021-01-17 15:00:49 -0500

    printing local worker grad norm for testing

--- tensorflow_estimator/python/estimator/estimator.py
+++ tensorflow_estimator/python/estimator/estimator.py
@@ -1589,14 +1589,15 @@ class Estimator(object):
               logging.info('@sahiltyagi4 test_worker norm ' + str(test_w_norm)
                            + ' on step val of ' + str(curr_global_step))
               test_flattened_grad = mon_sess.run(tf.get_default_graph().get_tensor_by_name('sahil_test_flattened:0'))
-              if curr_global_step == 2:
-                  logging.info('@sahiltyagi4 flattened tensor is ' + str(test_flattened_grad))
-                  time.sleep(10000000)
-
-
 
               logging.info('@sahiltyagi4 global_grad_norm is ' + str(global_grad_norm) + ' for global step '
                            + str(curr_global_step))
+
+              if curr_global_step == 2:
+                  logging.info('@sahiltyagi4 start printing flattened tensor')
+                  tf.print(test_flattened_grad, summarize=-1)
+                  logging.info('@sahiltyagi4 stop printing flattened tensor')
+                  time.sleep(10000000)
 
               if w_type == 'master':
                 grad_norm_window[int(curr_global_step)] = float(global_grad_norm)
